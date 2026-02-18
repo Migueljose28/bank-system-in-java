a38025be7650379df393970d0c8ea23ccbb9fbcd
migueljose28 <miguelneto275@gmail.com>
2026-02-18 14:35:20 -0300

    Initializer Spring Project: Let's go!

---
+++ src/main/java/com/two/bank_system/domain/User.java
@@ -0,0 +1,13 @@
+package com.two.bank_system.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String email;
+}
